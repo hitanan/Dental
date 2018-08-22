639a84a2e2c5b467a6fb3145a68097a52ad3a19e
hitanan <hitanan@gmail.com>
2018-08-22 16:12:50 +0700

    Apply ViewModel Code snippet for a property with INotifyPropertyChanged base Class Add DB file

---
+++ Dental-Lab/ViewModel/BaseViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Dental_Lab.ViewModel
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Sets the property.
+        /// </summary>
+        /// <returns><c>true</c>, if property was set, <c>false</c> otherwise.</returns>
+        /// <param name="backingStore">Backing store.</param>
+        /// <param name="value">Value.</param>
+        /// <param name="validateValue">Validates value.</param>
+        /// <param name="propertyName">Property name.</param>
+        /// <param name="onChanged">On changed.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        protected virtual bool SetProperty<T>(
+            ref T backingStore, T value,
+            [System.Runtime.CompilerServices.CallerMemberName]string propertyName = "",
+            Action onChanged = null,
+            Func<T, T, bool> validateValue = null)
+        {
+            //if value didn't change
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            //if value changed but didn't validate
+            if (validateValue != null && !validateValue(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+
+        /// <summary>
+        /// Occurs when property changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises the property changed event.
+        /// </summary>
+        /// <param name="propertyName">Property name.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+
+    class RelayCommand<T> : ICommand
+    {
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+
+        public RelayCommand(Predicate<T> canExecute, Action<T> execute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            try
+            {
+                return _canExecute == null ? true : _canExecute((T)parameter);
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ Dental-Lab/ViewModel/MainViewModel.cs
@@ -0,0 +1,88 @@
+﻿using Dental_Lab.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Globalization;
+
+namespace Dental_Lab.ViewModel
+{
+    class MainViewModel : BaseViewModel
+    {
+        public ICommand CloseCommand { get; set; }
+        public ICommand ToggleCommand { get; set; }
+        public ICommand ToggleMenuItemCommand { get; set; }
+        private bool _ToggleIsCheched;
+        public bool ToggleIsCheched
+        {
+            get => _ToggleIsCheched;
+            set => SetProperty(ref _ToggleIsCheched, value);
+        }
+
+        private string _MenuSelected;
+        private string MenuSelected
+        {
+            get =>_MenuSelected;
+            set => SetProperty(ref _MenuSelected, value);
+        }
+
+        public MainViewModel()
+        {
+            CloseCommand = new RelayCommand<Window>(null, (w) => { w.Close(); });
+            ToggleCommand = new RelayCommand<Window>(null, (w) => ToggleMenuAction(w));
+            ToggleMenuItemCommand = new RelayCommand<object>(null, (obj) =>
+            {
+                ToggleIsCheched = !ToggleIsCheched;
+
+                var values = (object[])obj;
+                var window = (Window)values[0];
+                var SelectedItem = (ListViewItem)values[1];
+                var GridMain = (Grid)values[2];
+
+                ToggleMenuAction(window);
+
+                UserControl usc = null;
+                GridMain.Children.Clear();
+
+                switch (SelectedItem.Name)
+                {
+                    case "ItemHome":
+                        usc = new Client();
+                        GridMain.Children.Add(usc);
+                        break;
+                    case "ItemCreate":
+                        usc = new Client();
+                        GridMain.Children.Add(usc);
+                        break;
+                    default:
+                        break;
+                }
+
+            });
+        }
+        private void ToggleMenuAction(Window w)
+        {
+            var sb = (Storyboard)w.FindResource(ToggleIsCheched ? "OpenMenu" : "CloseMenu");
+            sb.Begin();
+        }
+    }
+
+    public class MultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.Clone();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
